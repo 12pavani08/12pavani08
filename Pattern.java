8c56c583905b6e78f0a6a5d770023ed6491c3ea4
12pavani08 <86842838+12pavani08@users.noreply.github.com>
2021-07-03 22:46:18 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,24 @@
+class Pattern
+{
+  public static void main(String []arts)
+  for(int i=1;i<=5;i++)
+   {
+      for(int j=1;j<=i;j++)
+       {
+          System.out.println(" * ");
+            {
+                System.out.println(" ");
+             }
+        }
+    }
+        for (int i=4;i>=0;i--)
+         {
+            for(int j=1;j<=i;j++)
+             {
+                  System.out.println(" * ");
+                    {
+                       System.out.println(" ");
+                     }
+              }
+          }
+}
